69b8a69044f8088890fa0240de04238902bc3d83
firman <novianicesarfirdaus2@gmail.com>
2025-08-24 18:06:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,120 +1,72 @@
-let provider, signer, stakingContract;
+const stakingAddress = "0x8166fd5fab2e7397ea29cab15371a1caf0fd63f0";
+const tokenAddress = "0x6ce343073c191560b018dc5eaab6568945ba1e90";
 
-const stakingAddress = "0x5393aBE6CE6fF2BD32fb4FA962D75B9cA3ae07D3"; // ganti dgn address staking contract
+// ABI staking contract (dari kamu)
 const stakingABI = [
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "transfer",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "from",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "transferFrom",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-]
-  // contoh minimal ABI
-  "function stake(uint256 amount) public",
-  "function unstake(uint256 amount) public",
-  "function getStake(address user) public view returns (uint256)"
+  { "inputs": [], "name": "claim", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+  { "inputs": [{ "internalType": "uint256", "name": "_amount", "type": "uint256" }], "name": "stake", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+  { "inputs": [{ "internalType": "uint256", "name": "_amount", "type": "uint256" }], "name": "unstake", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+  { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
+  { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }], "name": "pendingReward", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }
 ];
 
-async function connectWallet() {
+// Minimal ERC20 ABI untuk approve
+const tokenABI = [
+  { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "", "type": "bool" }], "type": "function" }
+];
+
+let web3;
+let accounts;
+let stakingContract;
+let tokenContract;
+
+document.getElementById("connectButton").onclick = async () => {
   if (window.ethereum) {
-    provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send("eth_requestAccounts", []);
-    signer = provider.getSigner();
-    const address = await signer.getAddress();
-    document.getElementById("walletAddress").innerText = "Connected: " + address;
+    web3 = new Web3(window.ethereum);
+    accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    stakingContract = new web3.eth.Contract(stakingABI, stakingAddress);
+    tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
 
-    // load contract
-    stakingContract = new ethers.Contract(stakingAddress, stakingABI, signer);
-
-    // tampilkan menu staking
-    document.getElementById("stakingMenu").style.display = "block";
-
-    // update info stake user
-    updateStakeInfo(address);
+    document.getElementById("walletAddress").innerText = accounts[0];
+    document.getElementById("app").style.display = "block";
+    updateInfo();
   } else {
-    alert("MetaMask tidak terdeteksi!");
+    alert("Please install MetaMask!");
   }
+};
+
+async function updateInfo() {
+  if (!stakingContract) return;
+  const staked = await stakingContract.methods.balanceOf(accounts[0]).call();
+  const reward = await stakingContract.methods.pendingReward(accounts[0]).call();
+
+  document.getElementById("stakedBalance").innerText = web3.utils.fromWei(staked, "ether");
+  document.getElementById("pendingReward").innerText = web3.utils.fromWei(reward, "ether");
 }
 
-async function stakeTokens() {
+document.getElementById("stakeButton").onclick = async () => {
   const amount = document.getElementById("stakeAmount").value;
-  try {
-    const tx = await stakingContract.stake(ethers.utils.parseEther(amount));
-    await tx.wait();
-    alert("✅ Staking berhasil!");
-    updateStakeInfo(await signer.getAddress());
-  } catch (err) {
-    console.error(err);
-    alert("❌ Gagal staking: " + err.message);
-  }
-}
+  if (!amount) return alert("Enter amount");
 
-async function unstakeTokens() {
-  const amount = document.getElementById("stakeAmount").value;
-  try {
-    const tx = await stakingContract.unstake(ethers.utils.parseEther(amount));
-    await tx.wait();
-    alert("✅ Unstake berhasil!");
-    updateStakeInfo(await signer.getAddress());
-  } catch (err) {
-    console.error(err);
-    alert("❌ Gagal unstake: " + err.message);
-  }
-}
+  const weiAmount = web3.utils.toWei(amount, "ether");
 
-async function updateStakeInfo(user) {
-  try {
-    const stake = await stakingContract.getStake(user);
-    document.getElementById("stakeInfo").innerText =
-      "Staked: " + ethers.utils.formatEther(stake) + " $tAIR";
-  } catch (err) {
-    console.error(err);
-  }
-}
+  await tokenContract.methods.approve(stakingAddress, weiAmount).send({ from: accounts[0] });
+  await stakingContract.methods.stake(weiAmount).send({ from: accounts[0] });
+
+  updateInfo();
+};
+
+document.getElementById("unstakeButton").onclick = async () => {
+  const amount = document.getElementById("unstakeAmount").value;
+  if (!amount) return alert("Enter amount");
+
+  const weiAmount = web3.utils.toWei(amount, "ether");
+
+  await stakingContract.methods.unstake(weiAmount).send({ from: accounts[0] });
+  updateInfo();
+};
+
+document.getElementById("claimButton").onclick = async () => {
+  await stakingContract.methods.claim().send({ from: accounts[0] });
+  updateInfo();
+};
